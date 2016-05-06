f94613c4c5e196229e223d1d5aff1f5cd247e520
rycool <ryanmorris@optonline.net>
2016-05-06 15:10:03 -0400

    Update sde.js

--- sde.js
+++ sde.js
@@ -84,7 +84,7 @@ var sde = (function(){
 
 	var getEmoteList = function(callback){
 		return $.ajax({
-			url: "https://ahiddenwaffle.github.io/secretdungeonemotes/dungeonemotes-prefixed.json",
+			url: "https://rycool.github.io/secretdungeonemotes/dungeonemotes-prefixed.json",
 			dataType: "jsonp",
 			jsonpCallback: "sde_jsonp_static"
 		}).done(callback).fail(function(jqXHR, textStatus, errorThrown){
